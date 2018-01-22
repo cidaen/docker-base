950298e78d73b3b80bc7eaee1516f41598267de9
Enrique G. Rodrigo <Enrique.GRodrigo@gmail.com>
2018-01-22 18:48:53 +0100

    Makes workdir static

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -8,7 +8,8 @@ c.NotebookApp.port = 8888
 c.NotebookApp.allow_root = True
 c.NotebookApp.open_browser = False
 
-c.NotebookApp.notebook_dir = os.getenv("NOTEBOOK_DIR","")
+#c.NotebookApp.notebook_dir = os.getenv("NOTEBOOK_DIR","")
+c.NotebookApp.notebook_dir = "/home/work/project" 
 
 # No password at the begining
 c.NotebookApp.token = ''
